b4456e7308cff473259368414ddc08bb036b483c
Jasper Spahl <jasperspahl@web.de>
2022-12-02 00:18:04 +0100

    Day01

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -1,15 +1,30 @@
-﻿namespace AdventOfCode;
+namespace AdventOfCode;
 
 public class Day01 : BaseDay
 {
-    private readonly string _input;
+    private readonly List<string> _input;
+    private readonly List<int> _elves;
 
     public Day01()
     {
-        _input = File.ReadAllText(InputFilePath);
+        _input = File.ReadAllLines(InputFilePath).ToList();
+        _elves = new List<int> {0};
+        var curr = 0;
+        foreach (var line in _input)
+        {
+            if (line == "")
+            {
+                curr++;
+                _elves.Add(0);
+            }
+            else
+            {
+                _elves[curr] += int.Parse(line);
+            }
+        }
     }
-
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
-
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    
+    public override ValueTask<string> Solve_1() => new($"{_elves.Max()}");
+    
+    public override ValueTask<string> Solve_2() => new($"{_elves.OrderByDescending(n=>n).Take(3).Sum()}");
 }
--- AdventOfCode/Program.cs
+++ AdventOfCode/Program.cs
@@ -1,18 +1,20 @@
-if (args.Length == 0)
+switch (args.Length)
 {
-    await Solver.SolveLast(opt => opt.ClearConsole = false);
-}
-else if (args.Length == 1 && args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase))
-{
-    await Solver.SolveAll(opt =>
+    case 0:
+        await Solver.SolveLast(opt => opt.ClearConsole = false);
+        break;
+    case 1 when args[0].Contains("all", StringComparison.CurrentCultureIgnoreCase):
+        await Solver.SolveAll(opt =>
+        {
+            opt.ShowConstructorElapsedTime = true;
+            opt.ShowTotalElapsedTimePerDay = true;
+        });
+        break;
+    default:
     {
-        opt.ShowConstructorElapsedTime = true;
-        opt.ShowTotalElapsedTimePerDay = true;
-    });
-}
-else
-{
-    var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
+        var indexes = args.Select(arg => uint.TryParse(arg, out var index) ? index : uint.MaxValue);
 
-    await Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+        await Solver.Solve(indexes.Where(i => i < uint.MaxValue));
+        break;
+    }
 }
